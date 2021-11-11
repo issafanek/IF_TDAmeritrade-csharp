d7010cfda61597bf0ef679c533a721b1becbffae
Issa Fanek <issa.fanek@gmail.com>
2021-11-11 12:01:21 -0500

    Added license file to root folder

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IF_TDAmeritrade_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
